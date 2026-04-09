1c4c74f1817805b9af769062d975e2e01f55fc29
Wen-Chuang Chou <wen-chuang.chou@tu-dresden.de>
2026-04-09 10:16:00 +0200

    feat: add SLURM orchestration script for launching vLLM server and then agentic Rag

--- hpc/download_model.sh
+++ hpc/download_model.sh
@@ -3,5 +3,5 @@
 module load release/25.06 GCCcore/13.3.0 Python/3.12.3 CUDA/13.0.0
 python -m venv --system-site-packages .venv
 source .venv/bin/activate
-hf download zai-org/GLM-4.7-Flash --exclude "*.bin"
+hf download Qwen/Qwen3.5-122B-A10B-FP8 --exclude "*.bin"
 #huggingface-cli download zai-org/GLM-4.7-FP8 --exclude "*.bin"
